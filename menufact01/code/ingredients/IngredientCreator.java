ea68284d7e0282e7af2212a01af151fd69512541
Franky55 <43887787+Franky55@users.noreply.github.com>
2024-10-07 13:56:52 -0400

    throw error for  negative value for ingredients

--- menufact01/code/ingredients/IngredientCreator.java
+++ menufact01/code/ingredients/IngredientCreator.java
@@ -2,18 +2,14 @@ package ingredients;
 
 public class IngredientCreator {
 
-    CreateNewIngredient(TypeIngredient typeIngredient){
-        switch (typeIngredient){
-            case FRUIT:
-                return new IngredientFruits();
-            case LEGUME:
-                return new IngredientLegumes();
-            case VIANDE:
-                return new IngredientViande();
-            case LAITIER:
-                return new IngredientLaitier();
-            case EPICE:
-                return new IngredientEpice();
-        }
+    public IIngredients CreateNewIngredient(TypeIngredient typeIngredient){
+        return switch (typeIngredient) {
+            case FRUIT -> new IngredientFruits();
+            case LEGUME -> new IngredientLegumes();
+            case VIANDE -> new IngredientViande();
+            case LAITIER -> new IngredientLaitier();
+            case EPICE -> new IngredientEpice();
+            default -> null;
+        };
     }
 }
