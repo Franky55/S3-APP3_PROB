db976c0b8e1324460c1175d1ca9d90b355dd18f5
LyamBRS <cous5830@gmail.com>
2024-10-08 10:34:33 -0400

    Testy

--- menufact01/code/gestionnaire/GestionnaireTest.java
+++ menufact01/code/gestionnaire/GestionnaireTest.java
@@ -53,6 +53,40 @@ class GestionnaireTest {
     }
 
     @Test
+    void ajoutePlatAFactureMultiple() {
+        Facture facture = new Facture("bruh bruh bruh");
+
+        Inventaire inventaire = Inventaire.getInstance();
+        inventaire.addIngredient(IngredientCreator.CreateNewIngredient(TypeIngredient.FRUIT, 2, "g"));
+        inventaire.addIngredient(IngredientCreator.CreateNewIngredient(TypeIngredient.EPICE, 2, "g"));
+        inventaire.addIngredient(IngredientCreator.CreateNewIngredient(TypeIngredient.LAITIER, 2, "ml"));
+        inventaire.addIngredient(IngredientCreator.CreateNewIngredient(TypeIngredient.LEGUME, 2, "g"));
+        inventaire.addIngredient(IngredientCreator.CreateNewIngredient(TypeIngredient.VIANDE, 2, "g"));
+
+        ArrayList<IIngredients> recette = new ArrayList<>(List.of(Objects.requireNonNull(IngredientCreator.CreateNewIngredient(TypeIngredient.FRUIT, 2, "g"))));
+        recette.add(IngredientCreator.CreateNewIngredient(TypeIngredient.VIANDE, 2, "g"));
+        recette.add(IngredientCreator.CreateNewIngredient(TypeIngredient.LAITIER, 2, "ml"));
+        recette.add(IngredientCreator.CreateNewIngredient(TypeIngredient.LEGUME, 2, "g"));
+        recette.add(IngredientCreator.CreateNewIngredient(TypeIngredient.EPICE, 2, "g"));
+
+        PlatAuMenu shrooms = new PlatAuMenu(0,"Recette vraiment élaboré",100, recette);
+        PlatChoisi iWantShrooms = new PlatChoisi(shrooms, 2);
+
+        Gestionnaire gestionnaire = new Gestionnaire(facture, inventaire);
+
+        assertTrue(gestionnaire.ajoutePlatAFacture(iWantShrooms));
+        assertFalse(gestionnaire.ajoutePlatAFacture(iWantShrooms));
+
+        assertEquals(0, inventaire.getIngredient(TypeIngredient.VIANDE).GetQuantiteRestant());
+        assertEquals(0, inventaire.getIngredient(TypeIngredient.EPICE).GetQuantiteRestant());
+        assertEquals(0, inventaire.getIngredient(TypeIngredient.LAITIER).GetQuantiteRestant());
+        assertEquals(0, inventaire.getIngredient(TypeIngredient.FRUIT).GetQuantiteRestant());
+        assertEquals(0, inventaire.getIngredient(TypeIngredient.LEGUME).GetQuantiteRestant());
+
+        assertEquals(EtatsPlat.IMPOSSIBLE, iWantShrooms.getEtatsPlat());
+    }
+
+    @Test
     void ajoutePlatChoisiUnPeuWeird() {
         Facture facture = new Facture("bruh bruh bruh");
 
