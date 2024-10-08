c7ade1097e70621ff914ff1bc24fd61184c5a4a6
LyamBRS <cous5830@gmail.com>
2024-10-07 23:13:15 -0400

    Doc pour ingredient

--- menufact01/code/ingredients/Ingredient.java
+++ menufact01/code/ingredients/Ingredient.java
@@ -5,26 +5,52 @@ public class Ingredient {
     private String description;
     private TypeIngredient typeIngredient;
 
+    /**
+     * get le nom de l'ingredient
+     * @return
+     */
     public String getNom() {
         return nom;
     }
 
+    /**
+     * set le nom de l'ingredient
+     * @param nom
+     */
     public void setNom(String nom) {
         this.nom = nom;
     }
 
+    /**
+     * get la description de l'ingredient
+     * @return
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Set la description de l'ingredient
+     * @param description
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * Retourne le type de l'ingredient.
+     * Sert a savoir quel classe a ete fait par la factory btw
+     * @return
+     */
     public TypeIngredient getTypeIngredient() {
         return typeIngredient;
     }
 
+    /**
+     * Set le type de l'ingredient a quelque chose.
+     * A eviter d'utiliser
+     * @param typeIngredient
+     */
     public void setTypeIngredient(TypeIngredient typeIngredient) {
         this.typeIngredient = typeIngredient;
     }
