dde6965699ac335c6285d7004b8f9e7cf68cafcf
Franky55 <43887787+Franky55@users.noreply.github.com>
2024-10-07 22:29:06 -0400

    class test Facture, et commentary

---
+++ menufact01/code/menufact/facture/FactureTest.java
@@ -0,0 +1,38 @@
+package menufact.facture;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FactureTest {
+
+    @org.junit.jupiter.api.Test
+    void associerClient() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sousTotal() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void total() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void payer() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void fermer() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void ouvrir() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getEtat() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void ajoutePlat() {
+    }
+}
